ebe1e2df6f864dd18c2369c6e2bf399f5b1b3c2e
Edufeed Brains <edufeedbrains@gmail.com>
2025-10-19 00:32:17 +0100

    Commit 4

--- src/app/components/app_features.js
+++ src/app/components/app_features.js
@@ -59,7 +59,7 @@ const AppFeatures = () => {
             </p>
           </div>
 
-          <div className={styles.card} style={{marginBottom: 30}}>
+          <div className={styles.card} style={{marginBottom: 50}}>
             <div className={styles.iconWrapper}>
               <span className={`material-symbols-outlined ${styles.icon}`}>
                 menu_book
