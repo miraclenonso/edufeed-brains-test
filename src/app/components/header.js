30619edae97a5d69ae4cd13538958c6f9bc04699
Edufeed Brains <edufeedbrains@gmail.com>
2025-10-20 03:20:12 +0100

    Commit 4

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,23 +1,26 @@
 "use client";
 
-import styles from './header.module.css'
-import Image from 'next/image'
+import { useState } from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logoSection}>
           <Image
             src="https://iili.io/KvpPbON.png"
-            alt='Logo'
+            alt="Logo"
             width={140}
             height={33}
             className={styles.logoImage}
           />
         </div>
 
-        <nav className={styles.nav}>
+        <nav className={`${styles.nav} ${menuOpen ? styles.showMenu : ""}`}>
           <a href="#">Home</a>
           <a href="#">Blog</a>
           <a href="#">About Us</a>
@@ -25,16 +28,27 @@ export default function Header() {
         </nav>
 
         <div className={styles.actionSection}>
-          <a href="#" className={styles.downloadBtn}>Download App</a>
+          <a href="#" className={styles.downloadBtn}>
+            Download App
+          </a>
           <Image
             src="https://iili.io/K8HJc9S.png"
-            alt='menu icon'
+            alt="menu icon"
             width={24}
             height={24}
             className={styles.menuImage}
+            onClick={() => setMenuOpen(!menuOpen)}
           />
         </div>
       </div>
+
+      {/* Overlay dropdown menu for mobile */}
+      <div className={`${styles.mobileMenu} ${menuOpen ? styles.menuOpen : ""}`}>
+        <a href="#">Home</a>
+        <a href="#">Blog</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact</a>
+      </div>
     </header>
-  )
+  );
 }
